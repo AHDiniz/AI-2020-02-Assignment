e8cf1548401e9264d31574a0b241239ffda3cf82
Alan H. Diniz <alanhdiniz@gmail.com>
2021-03-27 00:09:31 -0300

    Starting the graphics module

--- graphics.py
+++ graphics.py
@@ -1,5 +1,31 @@
 #!/usr/bin/python3.8
 
 import seaborn as sns
+import matplotlib.pyplot as plt
+
+def five_best_train_config_table(training_result):
+    
+
+def training_time_boxplots(training_result):
+    return None
+
+def training_avg_sse_boxplots(training_result):
+    return None
+
+def training_ranking_table(training_result):
+    return None
+
+def training_best_config_table(training_result):
+    return None
+
+
+def test_statistic_data_table(testing_result):
+    return None
+
+def testing_time_boxplots(testing_result):
+    return None
+
+def testing_avg_sse_boxplots(testing_result):
+    return None
 
 
